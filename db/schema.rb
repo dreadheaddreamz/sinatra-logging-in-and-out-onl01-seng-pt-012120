dfce3ee65365a171a7ec85b6936829fa935b47fa
James Batsell <jamesbatsell05@gmail.com>
2020-04-26 19:34:44 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200426184851) do
+ActiveRecord::Schema.define(version: 20200426193344) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.decimal "balance"
+  end
 
 end
